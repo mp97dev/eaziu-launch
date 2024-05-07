bec63baf2eacb13e716f795527ed0475b0f377cc
mp97dev <michipase@gmail.com>
2024-05-07 09:09:59 +0200

    update

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+colprint () {
+    echo -e "$1$2\033[0m"
+}
+
+colprint "\033[0;36m" "[II] Update ubuntu"
 sudo apt update
 sudo apt install python3 -y
 
-# install chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+colprint "\033[0;36m" "[II] Chrome install"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -q
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
-python3 -m venv venv
-source venv/bin/activate
+colprint "\033[0;36m" "[II] Install pip requirements"
 pip3 install -r requirements.txt
-echo "DONE!"
