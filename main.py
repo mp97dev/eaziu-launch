0b0cea8a0b4c22fa193a3d6b5131910b4badf6c1
gttrcr <gttrcr@gmail.com>
2024-08-07 22:41:17 +0200

    We did everything we could, but we failed

--- main.py
+++ main.py
@@ -13,12 +13,6 @@ from time import sleep
 config = []
 poke_substrings = ['poke', 'pokè', 'poké']
 
-def log(msg):
-    if not config["keep_the_secret"]:
-        print(msg)
-    else:
-        print("❔❔❔❔❔❔")
-
 def setup():
     print("Setting up webdriver...")
     options = webdriver.ChromeOptions()
@@ -30,89 +24,81 @@ def setup():
 def next_section():
     sleep(1)
     next_button = driver.find_elements(By.XPATH, "//*[@id=\"stkv-form-root\"]/div[4]/div/div[2]/div[1]/div/div/nav/button[1]")
-    if len(next_button) <= 0:
-        raise Exception("Missing next section button")
+    if len(next_button) == 0:
+        return False
     next_button[0].click()
     return True
 
 def pick_main_course():
-    print("🍝 Picking main course: ", end="")
+    print("🍝 Picking main course")
     sleep(1)
     picks = driver.find_elements(By.XPATH, "//*[@id=\"block-dbed4899-9130-4194-8098-218dd064d7bf\"]/div/div/div/div/div/div/div/div[2]/div/ul")
-    if len(picks) != 1:
+    if len(picks) == 0:
         return False
-    picks = picks[0].find_elements(By.XPATH, './*')
     pick = random.choice(picks)
     pick.click()
-    log(pick.get_attribute("aria-label"))
     return True
 
 def pick_follow():
     print("🍖 Picking following: ", end="")
     sleep(1)
     picks = driver.find_elements(By.XPATH, "//*[@id=\"block-d94294cc-5d4b-4d87-b2e4-af6e7b8093ba\"]/div/div/div/div/div/div/div/div[2]/div/ul")
-    if len(picks) != 1:
+    if len(picks) == 0:
         return False
-    picks = picks[0].find_elements(By.XPATH, './*')
     remove_every_poke = [elem for elem in picks if not any(x in elem.get_attribute("innerHTML").lower() for x in poke_substrings)]
     pick = random.choice(remove_every_poke)
     pick.click()
-    log(pick.get_attribute("aria-label"))
     return True
 
 def pick_poke():
-    print("🥑 Picking poke: ", end="")
+    print("🥑 Picking poke")
     sleep(1)
     picks = driver.find_elements(By.XPATH, "//*[@id=\"block-d94294cc-5d4b-4d87-b2e4-af6e7b8093ba\"]/div/div/div/div/div/div/div/div[2]/div/ul")
-    if len(picks) != 1: 
+    if len(picks) == 0:
         return False
-    picks = picks[0].find_elements(By.XPATH, './*')
     get_every_poke = [elem for elem in picks if any(x in elem.get_attribute("innerHTML").lower() for x in poke_substrings)]
     if(len(get_every_poke) <= 0):
         return False
     pick = random.choice(get_every_poke)
     pick.click()
-    log(pick.get_attribute("aria-label"))
     return True
 
 def pick_side():
-    print("🍅 Picking side: ", end="")
+    print("🍅 Picking side")
     sleep(1)
     picks = driver.find_elements(By.XPATH, "//*[@id=\"block-120ded4b-5c30-483d-959d-44d36ed05eed\"]/div/div/div/div/div/div/div/div[2]/div/ul")
-    if len(picks) != 1:
+    if len(picks) == 0:
         return False
     picks = picks[0].find_elements(By.XPATH, './*')
     pick = random.choice(picks)
     pick.click()
-    log(pick.get_attribute("aria-label"))
     return True
 
 def pick_fry():
-    print("🍤 Picking fry: ", end="")
+    print("🍤 Picking fry")
     sleep(1)
     picks = driver.find_elements(By.XPATH, "//*[@id=\"block-bc594af9-461e-4741-b8f5-59a7b54bd861\"]/div/div/div/div/div/div/div/div[2]/div/ul")
-    if len(picks) != 1:
+    if len(picks) == 0:
         return False
     picks = picks[0].find_elements(By.XPATH, './*')
     pick = random.choice(picks)
     pick.click()
-    log(pick.get_attribute("aria-label"))
     return True
 
 def set_notes():
     print("📓 Setting notes: " + config["note"])
     sleep(1)
     note_textarea = driver.find_elements(By.XPATH, "/html/body/div[3]/main/div[1]/div/div[2]/div[3]/div[3]/div/div/div/div/div/div[2]/section/div/div/div/div/div/div/div/div/div[2]/div[1]/div/textarea")
-    if len(note_textarea) <= 0:
-        raise Exception("Missing notes textbox")
+    if len(note_textarea) == 0:
+        return False
     note_textarea[0].send_keys(config["note"])
     return True
 
 def confirm():
     sleep(1)
     confirm_button = driver.find_elements(By.XPATH, "/html/body/div[3]/main/div[1]/div/div[2]/div[3]/div[3]/div/div/div/div/div/div[2]/section/div/div/div/div/div/div/div/div/div[2]/div[3]/div/div/div/div/div[1]/div/div/button")
-    if len(confirm_button) <= 0:
-        raise Exception("Missing confirm button")
+    if len(confirm_button) == 0:
+        return False
     confirm_button[0].click()
     sleep(1)
     print("✅ Confirmed")
@@ -131,7 +117,7 @@ if __name__ == "__main__":
         weights = list(config['weight'].values())
     
     ret = True
-    for i in range(0, 3):
+    for i in range(0, 5):
         print(f"🪖 Attempt n.{i + 1}")
         
         # Open website
@@ -140,50 +126,53 @@ if __name__ == "__main__":
 
         pick = random.choices(samples, weights=weights)[0]
         print("🎲 SELECTED: ", end="")
-        log(pick)
+        if config["keep_the_secret"]:
+            print("❔❔❔❔")
+        else:
+            print(pick)
 
         # Start
         start_button = driver.find_elements(By.XPATH, "//*[@id=\"root\"]/main/div[1]/div/div[2]/div[2]/div/section/div[1]/div/div/div/div/div/div[3]/div/div/div/button")
         if len(start_button) <= 0:
-            raise Exception("Missing Start button")
+            continue
         start_button[0].send_keys(Keys.ENTER)
 
         # Name
         print("🏌️‍♂️ Setting name: " + config["name"])
         name_input = driver.find_elements(By.XPATH, "//*[@id=\"block-ba44ca44-825f-449c-8809-ac604b19880f\"]/div/div/div/div/div/div/div/div[2]/div[1]/input")
         if len(name_input) <= 0:
-            raise Exception("Missing name input")
+            continue
         name_input[0].send_keys(config["name"])
         name_input[0].send_keys(Keys.ENTER)
 
         ret = True
         if pick == "primo":
-            ret &= pick_main_course()   # ✅ main
-            ret &= next_section()      # ❌ follow
-            ret &= next_section()      # ❌ fry
-            ret &= pick_side()         # ✅ side
-            ret &= next_section()      # ❌ water
+            if ret: ret &= pick_main_course()   # ✅ main
+            if ret: ret &= next_section()      # ❌ follow
+            if ret: ret &= next_section()      # ❌ fry
+            if ret: ret &= pick_side()         # ✅ side
+            if ret: ret &= next_section()      # ❌ water
         elif pick == "secondo":
-            ret &= next_section()  # ❌ main
-            ret &= pick_follow()   # ✅ follow
-            ret &= next_section()  # ❌ fry
-            ret &= pick_side()     # ✅ side 
-            ret &= next_section()  # ❌ water
+            if ret: ret &= next_section()  # ❌ main
+            if ret: ret &= pick_follow()   # ✅ follow
+            if ret: ret &= next_section()  # ❌ fry
+            if ret: ret &= pick_side()     # ✅ side 
+            if ret: ret &= next_section()  # ❌ water
         elif pick == "poke":
-            ret &= next_section()  # ❌ main
-            ret &= pick_poke()     # ✅ follow
-            ret &= next_section()  # ❌ fry
-            ret &= next_section()  # ❌ side 
-            ret &= next_section()  # ❌ water
+            if ret: ret &= next_section()  # ❌ main
+            if ret: ret &= pick_poke()     # ✅ follow
+            if ret: ret &= next_section()  # ❌ fry
+            if ret: ret &= next_section()  # ❌ side 
+            if ret: ret &= next_section()  # ❌ water
         elif pick == "rosticceria":
-            ret &= next_section()  # ❌ main
-            ret &= next_section()  # ❌ follow
-            ret &= pick_fry()      # ✅ fry
-            ret &= pick_side()     # ✅ side 
-            ret &= next_section()  # ❌ water
+            if ret: ret &= next_section()  # ❌ main
+            if ret: ret &= next_section()  # ❌ follow
+            if ret: ret &= pick_fry()      # ✅ fry
+            if ret: ret &= pick_side()     # ✅ side 
+            if ret: ret &= next_section()  # ❌ water
 
-        ret &= set_notes()
-        ret &= confirm()
+        if ret: ret &= set_notes()
+        if ret: ret &= confirm()
 
         if ret:
             break
